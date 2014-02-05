9c1cc2a1f161e987219bf07dac2f89d1352ac78e
MikroMan <subic.tine@gmail.com>
2014-02-05 19:51:23 +0100

    Fibonacci Java
    Fibonacci program calculator

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.*; //import library for Scanner
+
+public class Fibonacci{
+	public static void main(String[] args){
+		System.out.print("Enter maximum number: ");
+		Scanner sc= new Scanner(System.in); //initializes scanner for console input
+		int n= sc.nextInt();
+		
+		int a=0;
+		int b=1;
+		int c=0;
+		
+		while (a+c<=n){
+			
+			a+=c;
+			c=b;
+			b=a;
+		}	
+		System.out.println(a);
+	}
+}
