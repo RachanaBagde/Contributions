126ed1bb3b4a0396f99ba9c523aadf887c60cce3
MikroMan <subic.tine@gmail.com>
2014-02-06 10:14:02 +0100

    More ministuff

---
+++ Cipher.java
@@ -0,0 +1,63 @@
+/*
+Caesar cipher by MikroMan
+========================
+Implements single encoding of entered text, can
+output all possible decrypted texts given an encoded text.
+*/
+
+import java.util.*;
+public class Cipher{
+	public static void main(String[] args){
+		char[] A = {'a','b','c','d','e','f','g','h','i','j','k','l',
+		'm','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter text to be encoded: ");
+		String text = sc.nextLine();
+		
+		System.out.print("Enter an integer for cypher: ");
+		int n= sc.nextInt();	
+		char[] c = text.toCharArray(); //entered text to an array of letters
+		char[] out = encodeText(c,n,A);
+		System.out.print("Encoded text: ");
+		printCode(out);
+		System.out.println();
+		System.out.println();
+		
+		String ignore = sc.nextLine();
+		System.out.print("Enter text to be decoded: ");
+		String textD = sc.nextLine();	
+		char[] dec = textD.toCharArray();
+		decodeText(dec, A);		
+	}
+	public static char[] encodeText(char[] c, int n,char[] A){
+		char[] o = new char[c.length]; //create an array to output encrypted message
+		for (int i=0; i<c.length;i++){
+			//if array elemnt is punc. mark or space, copy it
+			if (c[i]==' ' || c[i]=='.' || c[i]==',') o[i] = c[i]; 
+			//else, find in alphabet and shift for n
+			else {
+				alph: for (int j=0;j<A.length;j++){
+					if (A[j]==c[i]){
+					o[i] = A[(j+n)%A.length];
+					break alph;
+					}
+				}
+			}
+		}
+		return o;
+	}
+	public static void decodeText(char[] e,char[] A){
+		for (int i=1;i<25;i++){
+			char[] decode = encodeText(e,i,A);
+			printCode(decode);
+			System.out.print(" - Shifted by "+i+" letters.");
+			System.out.println();
+		}
+	}
+	public static void printCode(char[] out){
+		for (int i=0; i<out.length;i++){
+			System.out.print(out[i]);
+		}
+	}
+}
---
+++ TileCost.java
@@ -0,0 +1,30 @@
+/*
+Calculate the total cost of tile it would take to cover a floor 
+plan of width and height, using a cost entered by the user.
+Widthe and height are integers, cost of tiles is a decimal number
+*/
+
+import java.util.*; //import library for scanner
+
+public class TileCost{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter width of your floor plan: ");
+		int w = sc.nextInt();
+		
+		System.out.print("Enter height of your floor plan: ");
+		int h = sc.nextInt();
+		//Note: scanner for double (decimal number) will use your 
+		//default Locale to determine decimal separator (. or,).
+		//Uncomment the next line to change it to US locale, that uses . (dot)
+		//sc.useLocale(Locale.US);
+		
+		System.out.print("Enter cost of tiles per square meter: ");
+		double c = sc.nextDouble();
+		
+		System.out.println();
+		System.out.printf("Total cost of tile for %d square meters if %.2f EUR", h*w,h*w*c);
+		System.out.println();
+	}
+}
